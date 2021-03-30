c8d16fb2d1bef059eaf76450202046dcdbe0cc8f
mandla <mandlenkosi.khumalo@momentum.co.za>
2021-03-30 19:28:42 +0200

    Refactored the Repository classes to use JPA

--- anycompany/src/main/java/za.co.anycompany/datalayer/CustomerRepository.java
+++ anycompany/src/main/java/za.co.anycompany/datalayer/CustomerRepository.java
@@ -1,64 +1,30 @@
 package za.co.anycompany.datalayer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.co.anycompany.datalayer.dao.CustomerDAO;
 import za.co.anycompany.model.Customer;
 
-import java.sql.*;
+import java.util.List;
 
-
+@Service
 public class CustomerRepository {
 
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-    private static final String DB_USER = "";
-    private static final String DB_PASSWORD = "";
+    private static CustomerDAO customerDAO;
+
+    private CustomerDAO autowiredDAO;
+
+    @Autowired
+    private CustomerRepository(CustomerDAO customerDAO, CustomerDAO autowiredDAO) {
+        CustomerRepository.customerDAO = customerDAO;
+        this.autowiredDAO = autowiredDAO;
+    }
 
     public static Customer load(int customerId) {
-        Connection con = getDBConnection();
-        PreparedStatement prpstmt = null;
-        ResultSet resultSet = null;
-        Customer customer = new Customer();
-        try {
-            prpstmt = con.prepareStatement("select * from CUSTOMER where customerId = ?");
-            prpstmt.setInt(1, customerId);
-            resultSet = prpstmt.executeQuery();
-            while (resultSet.next()) {
-                customer.setName(resultSet.getString("NAME"));
-                customer.setCountry(resultSet.getString("COUNTRY"));
-                customer.setDateOfBirth(resultSet.getDate("DATE_OF_BIRTH"));
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (prpstmt != null)
-                    prpstmt.close();
-                if (resultSet != null)
-                    resultSet.close();
-                if (con != null)
-                    con.close();
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        return customer;
+        return customerDAO.getOne(customerId);
     }
 
-
-    private static Connection getDBConnection() {
-        Connection dbConnection = null;
-        try {
-            Class.forName(DB_DRIVER);
-        } catch (ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
-            return dbConnection;
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return dbConnection;
+    public List<Customer> loadAllCustomers(){
+        return autowiredDAO.findAll();
     }
-
 }
